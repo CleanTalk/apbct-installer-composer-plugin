2c699c2254ce49dd1810c04c46683c9ae2a85aaa
Glomberg <bazz@bk.ru>
2025-12-11 16:03:12 +0300

    Fix. Installer conditions fixed.

--- src/Installer.php
+++ src/Installer.php
@@ -10,24 +10,25 @@ class Installer extends LibraryInstaller
     /**
      * @inheritDoc
      */
-    public function supports($packageType)
-    {
-        return $packageType === 'cleantalk-apbct-lib';
-    }
-
-    /**
-     * @inheritDoc
-     */
     public function getInstallPath(PackageInterface $package)
     {
-        $name = explode('/', $package->getName());
-        $name = end($name);
-        $name = explode('-', $name);
-        $name_processed = [];
-        foreach ( $name as $name_item ) {
-            $name_processed[] = ucfirst($name_item);
+        $package_name = $package->getName();
+        $package_type = $package->getType();
+
+        if (
+            $package_type === 'cleantalk-apbct-lib' ||
+            strpos($package_name, 'cleantalk/') === 0
+        ) {
+            $name = explode('/', $package->getName());
+            $name = end($name);
+            $name = explode('-', $name);
+            $name_processed = [];
+            foreach ( $name as $name_item ) {
+                $name_processed[] = ucfirst($name_item);
+            }
+            return "lib/Cleantalk/Common/" . implode('', $name_processed);
         }
-        return "lib/Cleantalk/Common/" . implode('', $name_processed);
+        return parent::getInstallPath($package);
     }
 
 }
